9a5739364347c75eb9f83c41b407eaadf1edee1b
MohammadParsa Mortazavi <127190407+mpmms@users.noreply.github.com>
2024-09-10 09:43:13 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function func1(){
     window.location.href = 'https://ble.ir/join/EurNFLutxd'
 }
 function tometal(){
-    window.location.href = 'https://mpmms.github.io/mc'
+    window.location.href = 'https://mpmms.github.io/mc.html'
 }
 function togit(){
     window.location.href = 'https://github.com/mpmms'
